4183164214cd24d8a637d18cecb2fc9467eb70f5
Qtrung474 <biunon83@gmail.com>
2025-11-13 23:27:55 +0700

    Update 04.c

--- 04.c
+++ 04.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    char text[] = "Hoc lap trinh C rat thu vi";
+    char str[] = "Hoc lap trinh C rat thu vi";
     char searchChar;
     int count = 0;
     int i; 
-    printf("Chuoi co san: %s\n", text);
+    printf("Chuoi co san: %s\n", str);
     printf("Nhap vao 1 ky tu de dem: ");
     scanf(" %c", &searchChar); 
-    for (i = 0; text[i] != '\0'; i++) {
-        if (text[i] == searchChar) {
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] == searchChar) {
             count++;
         }
     }
@@ -17,3 +17,4 @@ int main() {
 
     return 0;
 }
+
