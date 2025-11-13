b6384a6f61402d638d163500338b1d4bbe507a4a
Qtrung474 <biunon83@gmail.com>
2025-11-13 23:32:43 +0700

    06.c

--- 06.c
+++ 06.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    char text[] = "Hoc cong nghe thong tin rat vui!";
+    char str[] = "Hoc cong nghe thong tin rat vui!";
     int i;
     int letterCount = 0; //letterCount : so luong chu cai
-    printf("Chuoi goc la: %s\n", text);
-    for (i = 0; text[i] != '\0'; i++) {
+    printf("Chuoi goc la: %s\n", str);
+    for (i = 0; str[i] != '\0'; i++) {
         
-        if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z')) {
+        if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
             letterCount++; 
         }
     }
     printf("So luong ky tu la chu cai: %d\n", letterCount);
 
     return 0;
-}
