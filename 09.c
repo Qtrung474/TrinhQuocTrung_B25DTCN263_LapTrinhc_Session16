995f6e25f17224c7b6d76236fde432aa8110b9fc
Qtrung474 <biunon83@gmail.com>
2025-11-13 23:36:31 +0700

    09.c

--- 09.c
+++ 09.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    char text[] = "hello world"; 
+    char str[] = "hello world"; 
     char charToRemove;// charToRemove : ky tu can xoa
     int i, j;
-    printf("Chuoi goc: %s\n", text);
+    printf("Chuoi goc: %s\n", str);
     printf("Nhap vao 1 ky tu de xoa: ");
     scanf(" %c", &charToRemove);
-    for (i = 0, j = 0; text[i] != '\0'; i++) {
-        if (text[i] != charToRemove) {
-            text[j] = text[i];
+    for (i = 0, j = 0; str[i] != '\0'; i++) {
+        if (str[i] != charToRemove) {
+            str[j] = str[i];
             j++; 
         }
     }
 
-    text[j] = '\0';
-    printf("Chuoi sau khi xoa: %s\n", text);
+    str[j] = '\0';
+    printf("Chuoi sau khi xoa: %s\n", str);
     return 0;
 }
