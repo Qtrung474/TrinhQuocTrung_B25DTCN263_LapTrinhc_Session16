176d35103f8ea2160574fef64e332259129201a6
Qtrung474 <biunon83@gmail.com>
2025-11-13 23:26:52 +0700

    03.c

--- 03.c
+++ 03.c
@@ -2,15 +2,16 @@
 #include <string.h> 
 
 int main() {
-    char text[] = "12345";
-    int length = strlen(text);
+    char str[] = "12345";
+    int length = strlen(str);
     int i; 
-    printf("Chuoi goc: %s\n", text);
+    printf("Chuoi goc: %s\n", str);
     printf("Chuoi dao nguoc: ");
     for (i = length - 1; i >= 0; i--) {
-        printf("%c", text[i]);
+        printf("%c", str[i]);
     }
     printf("\n");
 
     return 0;
 }
+
