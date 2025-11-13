05a41b68746adfc4573ecad40ff5e83a19b3e618
Qtrung474 <biunon83@gmail.com>
2025-11-13 23:35:21 +0700

    08.c

--- 08.c
+++ 08.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    char text[] = "hello world";
+    char str[] = "hello world";
     int i; 
-    printf("Chuoi goc: %s\n", text);
-    if (text[0] >= 'a' && text[0] <= 'z') {
-        text[0] = text[0] - 32;
+    printf("Chuoi goc: %s\n", str);
+    if (str[0] >= 'a' && str[0] <= 'z') {
+        str[0] = str[0] - 32;
     }
-    for (i = 1; text[i] != '\0'; i++) {
+    for (i = 1; str[i] != '\0'; i++) {
         
-        if (text[i-1] == ' ') {
+        if (str[i-1] == ' ') {
             
-            if (text[i] >= 'a' && text[i] <= 'z') {
-                text[i] = text[i] - 32;
+            if (str[i] >= 'a' && str[i] <= 'z') {
+                str[i] = str[i] - 32;
             }
         }
     }
-    printf("Chuoi da viet hoa: %s\n", text);
+    printf("Chuoi da viet hoa: %s\n",str);
     return 0;
 }
