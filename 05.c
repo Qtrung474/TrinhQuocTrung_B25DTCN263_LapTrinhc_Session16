e3adec584201ce29af06016693ff7f934ff6551e
Qtrung474 <biunon83@gmail.com>
2025-11-13 23:29:35 +0700

    05.c

--- 05.c
+++ 05.c
@@ -2,15 +2,15 @@
 #include <string.h> 
 
 int main() {
-    char text[256];
+    char str[256];
     int soTu = 0;
     int inWord = 0; 
     int i;
     printf("Nhap vao mot chuoi: ");
-    fgets(text, sizeof(text), stdin);
-    text[strcspn(text, "\n")] = '\0';
-    for (i = 0; text[i] != '\0'; i++) {
-        if (text[i] == ' ') {
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';
+    for (i = 0;str[i] != '\0'; i++) {
+        if (str[i] == ' ') {
             inWord = 0; 
         }
         else if (inWord == 0) {
@@ -22,3 +22,4 @@ int main() {
 
     return 0;
 }
+
